fd457d933d401cd0166357dca39a1c2eafd06e27
Olixn <95512865+Olixn@users.noreply.github.com>
2022-07-14 18:09:29 +0800

    feat：接口sign

--- app/controller/Admin.php
+++ app/controller/Admin.php
@@ -9,12 +9,21 @@ use think\response\Json;
 
 class Admin extends BaseController
 {
+    protected $sign = 'ne-21!!@';
+
     public function AddCode(Request $request): Json
     {
         $code = $request->param('code');
+        $sign = $request->param('sign');
+
+
+        if (!$sign || $sign != md5($this->sign)) {
+            return json(['code'=>2]);
+        }
+
         $db = new CxCode();
         $res = $db->where('encode',$code)->select();
-        if ($res->isEmpty()) {
+        if ($res->isEmpty() && $code && strlen($code) == 32) {
             $db->insert(['encode'=>$code]);
             return json(['code'=>1]);
         }
